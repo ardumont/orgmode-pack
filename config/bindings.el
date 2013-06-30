79c01c3304c2c508592346bf9e21fd2d820f08b6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-30 14:41:09 +0200

    bindings

--- config/bindings.el
+++ config/bindings.el
@@ -1,1 +1,4 @@
 (define-key global-map (kbd "C-c c") 'org-capture)
+(define-key global-map (kbd "C-c l") 'org-store-link)
+(define-key global-map (kbd "C-c a") 'org-agenda)
+;;(define-key global-map (kbd "C-c b") 'org-iswitchb)
