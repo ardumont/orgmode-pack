405670604a9af751dd81dcc3defffc8a9a11f45b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-03 20:10:12 +0100

    Change the loading of lib policy

--- init.el
+++ init.el
@@ -114,7 +114,7 @@
 ;;;;;;;;; Math setup
 
 ;; there is trouble with the standard install so I use directly emacs-live's native api
-(live-add-pack-lib "ac-math")
+(load-file "lib/ac-math.el")
 
 (require 'ac-math)
 
