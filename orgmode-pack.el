dbda62acdea282216941669c70a2d678e83039f3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-11-08 13:42:03 +0100

    The patch delivered earlier did not fix the needed ert package.

--- orgmode-pack.el
+++ orgmode-pack.el
@@ -13,7 +13,9 @@
 (eval-after-load "org-toc-autoloads"
   '(progn
      (if (require 'org-toc nil t)
-         (add-hook 'org-mode-hook 'org-toc-enable)
+         (progn
+           (require 'ert)
+           (add-hook 'org-mode-hook 'org-toc-enable))
        (warn "org-toc not found"))))
 
 (require 'smartscan)
